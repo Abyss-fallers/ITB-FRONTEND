4ea595333ccf38fc72a23084f15de629383493d9
Daniil Oberlev <Switch.ne.lox@gmail.com>
2024-09-05 16:33:28 +0300

    TEST: delete comment

--- tools/remove_comments.cpp
+++ tools/remove_comments.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
 #include <filesystem>
 
 void removeComments(const std::string& filePath) {
-    std::ifstream inputFile(filePath);
+
+    std::filesystem::path path(filePath);
+
+    path = std::filesystem::absolute(path);
+
+    std::filesystem::path allowedDir1 = std::filesystem::absolute("src/");
+    std::filesystem::path allowedDir2 = std::filesystem::absolute("app/");
+
+    if (path.native().compare(0, allowedDir1.native().size(), allowedDir1.native()) != 0 &&
+        path.native().compare(0, allowedDir2.native().size(), allowedDir2.native()) != 0) {
+        throw std::runtime_error("Not allowed!");
+    }
+
+    std::ifstream inputFile(path);
     std::string outputFilePath = filePath + ".tmp";
     std::ofstream outputFile(outputFilePath);
 
@@ -40,7 +52,12 @@ int main(int argc, char* argv[]) {
     for (int i = 1; i < argc; ++i) {
         std::string filePath = argv[i];
         if (hasExtension(filePath, ".ts") || hasExtension(filePath, ".tsx") || hasExtension(filePath, ".js")) {
-            removeComments(filePath);
+            try {
+                removeComments(filePath);
+            }
+            catch (const std::exception& e) {
+                std::cerr << "Error processing file " << filePath << ": " << e.what() << std::endl;
+            }
         }
     }
     return 0;
